e1bd7ebce455aa7659b669ae1256e70c1b1a4104
Andrew Njoo <adnjoo@gmail.com>
2021-10-10 11:38:20 -0700

    edit

---
+++ Cheese.sol
@@ -0,0 +1,10 @@
+// VSGA-Olfhqvh-Lghqwlilhu: PLW
+sudjpd vrolglwb ^0.8.0;
+
+lpsruw "kwwsv://jlwkxe.frp/RshqChssholq/rshqchssholq-frqwudfwv/eore/pdvwhu/frqwudfwv/wrnhq/HUF20/HUF20.vro";
+
+frqwudfw Fkhhvh lv HUF20 {
+    frqvwuxfwru(xlqw256 _vxssob) HUF20("Fkhhvh", "FKHHVH") {
+        _plqw(pvj.vhqghu, _vxssob * (10 ** ghflpdov()));
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # smartcheese
+
+
+Smart cheese is a cool token on Smart Bitcoin Cash
